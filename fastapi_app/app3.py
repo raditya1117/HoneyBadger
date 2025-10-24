9d870b43d73dea27cd5919283ea986ae3bbc6ff7
Aditya Raj <rajadityagolu@gmail.com>
2025-10-25 00:20:58 +0530

    working on article

--- fastapi_app/app3.py
+++ fastapi_app/app3.py
@@ -1,38 +1,51 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
+from fastapi.responses import JSONResponse
 
-#uvicorn fastapi_app:app1 --reload --port 8080 --host 0.0.0.0
-# curl -X GET "http://127.0.0.1:8080/"
+
 app = FastAPI()
 
+# Define the root API endpoint
 @app.get("/")
-async def read_root():
-    return {"message": "Welcome."}
+async def root():
+    return JSONResponse(status_code=200, content={"type":"METADATA", "output": "Welcome to Calculator by HoneyBadger."})
 
 
 # Define the input data model
 class InputData(BaseModel):
-    num1: int
-    num2: int
+    num1: float
+    num2: float
     operation: str
 
+# Define the calculator API endpoint
 @app.post("/calculate/")
 async def calculation(input_data: InputData):
     num1=input_data.num1
     num2=input_data.num2
     operation=input_data.operation
     if operation=="add":
-        result=num1+num2
+        try:
+            result=num1+num2
+        except:
+            return JSONResponse(status_code=400, content={"type":"FAILURE", "reason":"Not able to add {} and {}.".format(num1, num2)})  
     elif operation=="subtract":
-        result=num1-num2
+        try:
+            result=num1-num2
+        except:
+            return JSONResponse(status_code=400, content={"type":"FAILURE", "reason":"Not able to subtract {} from {}.".format(num2, num1)})
     elif operation=="multiply":
-        result=num1*num2
+        try:
+            result=num1*num2
+        except:
+            return JSONResponse(status_code=400, content={"type":"FAILURE", "reason":"Not able to multiply {} and {}.".format(num1, num2)})
     elif operation=="divide":
-        result=num1/num2
+        try:
+            result=num1/num2
+        except:
+            return JSONResponse(status_code=400, content={"type":"FAILURE", "reason":"Not able to divide {} by {}.".format(num1, num2)})
     else:
-        raise HTTPException(status_code=404, detail={"type":"FAILURE", "reason":"Not a valid operation"})
+        result=None
     if result is None:
-        return {"type":"FAILURE", "reason":"Not a valid operation"}
+        raise HTTPException(status_code=404, detail={"type":"FAILURE", "reason":"Not a valid operation"})
     else:
-        return {"type":"SUCCESS", "output":result}
-        
+        return JSONResponse(status_code=200, content={"type":"SUCCESS", "output":result})
